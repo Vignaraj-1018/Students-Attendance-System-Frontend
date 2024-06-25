4ea454059467bdc60d53e1259fbcc44c685dda2b
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-06-25 22:57:16 +0530

    page layout setup - updte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserService } from './services/user-service/user.service';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
